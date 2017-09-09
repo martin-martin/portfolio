a42949e5b530585e0f28018a21c307da8b3145c7
martin-martin <breuss.martin@gmail.com>
2017-09-09 18:15:18 +0200

    feat: get data from dedicated file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -9,30 +10,8 @@ def home():
 
 @app.route('/portfolio')
 def portfolio():
-    projects = [
-        {
-            'name': 'xyayz',
-            'image': 'static/xyayz.png',
-            'description': 'this is my fun website.',
-            'live_link': 'http://www.xyayz.com',
-            'code_link': 'http://github.com/xyayz',
-            'info_link': 'http://medium.com'
-        },
-        {
-            'name': 'blossomizer',
-            'image': 'static/blossomizer.png',
-            'description': 'make crash blossoms.',
-            'live_link': 'https://d8z079h7ghotx.cloudfront.net/',
-            'code_link': 'https://github.com/martin-martin/blossomizer',
-            'info_link': 'https://medium.com/@martin.breuss/2-blossombuilder-e8304843a7f9'
-        },
-        {
-            'name': 'draft',
-            'image': '',
-            'description': 'in the works',
-            'code_link': 'https://github.com/martin-martin/blossomizer'
-        }
-    ]
+    with open('data/projects.json', 'r') as f:
+        projects = json.load(f)
     return render_template('portfolio.html',
                             title="portfolio",
                             projects=projects)
